1995e25cc48c92bff690a5cf5362644ac63d28b9
Shubham <sk5989229@gmail.com>
2026-07-30 02:18:12 +0530

    multer

--- backend/routes/musicRoute.js
+++ backend/routes/musicRoute.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const musicController = require('../controllers/musicController');
+const multer = require('multer');
 
 const router = express.Router();
 
 router.post('/upload', musicController.creatMusic);
-
 
 module.exports = router;
